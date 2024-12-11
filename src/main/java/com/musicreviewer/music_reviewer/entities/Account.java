2b6c18a44a6eff3f87164bf277eb37c4fcd5554d
R.Ø.M <rasmusorum@gmail.com>
2024-12-11 14:23:15 +0100

    kodeord væk

--- src/main/java/com/musicreviewer/music_reviewer/entities/Account.java
+++ src/main/java/com/musicreviewer/music_reviewer/entities/Account.java
@@ -5,7 +5,6 @@ import java.util.List;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -14,8 +13,6 @@ import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
-@Getter
-@Setter
 @Entity
 @Data
 public class Account {
