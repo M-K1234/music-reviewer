99a873665897e6529b989e4c3e51be96cdfded67
Arsim Ljoki <113034331+ArsimBoy420@users.noreply.github.com>
2024-12-11 15:29:01 +0100

    Account update

--- src/main/java/com/musicreviewer/music_reviewer/dtos/AccountDTO.java
+++ src/main/java/com/musicreviewer/music_reviewer/dtos/AccountDTO.java
@@ -1,5 +1,7 @@
 package com.musicreviewer.music_reviewer.dtos;
 
+import java.time.LocalDateTime;
+
 import com.musicreviewer.music_reviewer.entities.Account;
 
 import lombok.Getter;
@@ -8,31 +10,30 @@ import lombok.Setter;
 @Getter
 @Setter
 public class AccountDTO {
-    private int id_account;
-    private String creation_date;
-    private int reviews_created;
-    private int user_id_user;
-    private int login_id_login;
+    private int id; // Matcher "id" i Account
+    private String creationDate; // Bruger String, men husk at mappe fra LocalDateTime
+    private int reviewsCreated; // Matcher "reviewsCreated"
+    private int userId; // Refererer til "user" i Account
+    private int loginId; // Refererer til "login" i Account
     private int reviewCount; // Antal anmeldelser tilknyttet kontoen
 
     // Constructor for Account + reviewCount
     public AccountDTO(Account account, int reviewCount) {
-        this.id_account = account.getId_account();
-        this.creation_date = account.getCreation_date();
-        this.reviews_created = account.getReviews_created();
-        this.user_id_user = account.getUser_id_user();
-        this.login_id_login = account.getLogin_id_login();
+        this.id = account.getId();
+        this.creationDate = account.getCreationDate().toString(); // Konverter LocalDateTime til String
+        this.reviewsCreated = account.getReviewsCreated();
+        this.userId = account.getUser().getId(); // Hent user-id
+        this.loginId = account.getLogin().getId(); // Hent login-id
         this.reviewCount = reviewCount;
     }
 
     // Valgfri metode til at konvertere tilbage til en Account entity
     public Account toEntity() {
         Account account = new Account();
-        account.setId_account(this.id_account);
-        account.setCreation_date(this.creation_date);
-        account.setReviews_created(this.reviews_created);
-        account.setUser_id_user(this.user_id_user);
-        account.setLogin_id_login(this.login_id_login);
+        account.setId(this.id);
+        account.setCreationDate(LocalDateTime.parse(this.creationDate)); // Konverter String til LocalDateTime
+        account.setReviewsCreated(this.reviewsCreated);
+        // Bemærk: User og Login skal sættes med faktiske objekter
         return account;
     }
 }
