1142144bac1b73fc71a6b3b8c9c8d6ca632fb8cb
R.Ø.M <rasmusorum@gmail.com>
2024-12-12 13:09:26 +0100

    user change

--- src/main/java/com/musicreviewer/music_reviewer/entities/User.java
+++ src/main/java/com/musicreviewer/music_reviewer/entities/User.java
@@ -1,14 +1,10 @@
 package com.musicreviewer.music_reviewer.entities;
 
-import java.util.List;
-
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import lombok.Data;
 
